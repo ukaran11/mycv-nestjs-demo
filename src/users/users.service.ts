c76f3f18bda64bab6c5c329d7a31378b60158672
unknown <ukaran11@gmail.com>
2022-02-13 01:58:21 +0530

    Utsav | Added update service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -21,6 +21,14 @@ export class UsersService {
         return this.repo.find({ email })
     }
 
-    
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+        if(!user) {
+            throw new Error('User not found');
+        } 
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
 
 }
