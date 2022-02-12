e9e0262da14fa3c87bba8217a1dc6ab11dd454cc
unknown <ukaran11@gmail.com>
2022-02-13 01:58:40 +0530

    Utsav | Added remove service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -30,5 +30,12 @@ export class UsersService {
         return this.repo.save(user);
     }
 
-
+    
+    async remove(id: number) {
+        const user = await this.findOne(id);
+        if(!user) {
+            throw new Error('User not found');
+        }
+        return this.repo.remove(user);
+    }
 }
