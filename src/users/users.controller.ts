a1df41319a82380f48a7dbacf90fe336925266da
unknown <ukaran11@gmail.com>
2022-02-13 03:35:59 +0530

    Utsav | Added controller to delete a particular user based on param id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Delete, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dtos';
 import { UsersService } from './users.service';
 
@@ -14,5 +14,15 @@ export class UsersController {
     @Get('/:id')
     findUser(@Param('id') id: string){
         return this.usersService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id));
     }
 }
