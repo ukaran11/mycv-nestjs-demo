7759167beb8e6348ced079282afe53cd5c33588f
unknown <ukaran11@gmail.com>
2022-02-18 03:17:12 +0530

    Utsav | Built custom serializer interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,7 +4,7 @@ import { Body, Controller, Post, Get, Patch, Delete,
 import { CreateUserDto } from './dtos/create-user.dtos';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 @Controller('auth')
 export class UsersController {
     constructor(private usersService: UsersService) {}
@@ -14,9 +14,10 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
-    @UseInterceptors(ClassSerializerInterceptor)
+    @UseInterceptors(SerializeInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string){
+        console.log('handler is running');
         const user =await this.usersService.findOne(parseInt(id));
 
         if(!user) {
