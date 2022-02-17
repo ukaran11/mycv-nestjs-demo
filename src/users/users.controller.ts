85a30279a219841e42885bad40e0f71680282205
unknown <ukaran11@gmail.com>
2022-02-18 02:18:53 +0530

    Utsav | Excluded Response Properties

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Post, Get, Patch, Delete, Param, Query, NotFoundException } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Delete, 
+    Param, Query, NotFoundException, UseInterceptors, ClassSerializerInterceptor 
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dtos';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -12,6 +14,7 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
+    @UseInterceptors(ClassSerializerInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string){
         const user =await this.usersService.findOne(parseInt(id));
