9b6ebe6ae6c369494939f79553a3c30a67eee55b
unknown <ukaran11@gmail.com>
2022-02-20 03:48:51 +0530

    Utsav | Added cookie while signup/signin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,24 +16,18 @@ export class UsersController {
         private authService: AuthService
         ) {}
 
-    @Get('/colors/:color')
-    setColor(@Param('color') color:string, @Session() session:any){
-        session.color = color;
-    }
-
-    @Get('/colors')
-    getColor(@Session() session: any) {
-        return session.color;
-    }
-
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     @Post('/signin')
-    signin(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password);
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     // @UseInterceptors(new SerializeInterceptor(UserDto))
