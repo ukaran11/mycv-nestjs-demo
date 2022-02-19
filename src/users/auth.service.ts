16a6b24859abea2f4111c4de73ff146a3b48cff3
unknown <ukaran11@gmail.com>
2022-02-20 00:31:32 +0530

    Utsav | Hooked the signup service to user controller for user creation

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -25,9 +25,10 @@ export class AuthService {
         const result = salt + '.' + hash.toString('hex');
 
         // Create a new user and save it
+        const user = await this.usersService.create(email, result);
 
         // return the user
-
+        return user;
     }
 
     signin() {
