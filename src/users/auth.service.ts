342c17b1ea7874ecc8a228b8d756df47079e8d77
unknown <ukaran11@gmail.com>
2022-02-20 00:47:12 +0530

    Utsav | Handled signin

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { randomBytes, scrypt as _script } from "crypto";
 import { promisify } from "util";
@@ -31,7 +31,21 @@ export class AuthService {
         return user;
     }
 
-    signin() {
+    async signin(email: string, password: string) {
+        const [user] = await this.usersService.find(email);
 
+        if(!user) {
+            throw new NotFoundException('User Not Found');
+        }
+
+        const [salt, storedHash] = user.password.split('.');
+
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        if (storedHash !== hash.toString('hex')) {
+            throw new BadRequestException('Bad Password');
+        }
+        
+        return user;
     }
 }
