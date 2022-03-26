45316a113380439594e5e55713c1a7d99c6bb650
unknown <ukaran11@gmail.com>
2022-03-26 15:04:54 +0530

    Utsav | Created fakeUsersService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,11 +3,24 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 
 it('can create an instance of auth service', async () => {
+    // Create a fake copy of the users service
+    const fakeUsersService = {
+        find: () => Promise.resolve([]),
+        create: (email: string, password: string) => 
+            Promise.resolve({id: 1, email, password })
+    }
+
     const module = await Test.createTestingModule({
-        providers: [AuthService]
+        providers: [
+            AuthService,
+            {
+                provide: UsersService,
+                useValue: fakeUsersService
+            }
+        ]
     }).compile();
 
     const service = module.get(AuthService);
-
+    
     expect(service).toBeDefined();
 })
